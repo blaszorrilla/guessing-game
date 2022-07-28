17303ecd3099c01d4ec361bf771975729e941013
blaszla <blassalvador03@gmail.com>
2022-07-28 18:21:47 -0400

    game

---
+++ js/palabras.js
@@ -0,0 +1,166 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "lenguaje de programación con P"
+    },
+    {
+        word: "guitarra",
+        hint: "instrumento de cuerda"
+    },
+    {
+        word: "microsoft",
+        hint: "empresa de tecnología"
+    },
+    {
+        word: "venus",
+        hint: "Planeta del sistema solar"
+    },
+    {
+        word: "Oro",
+        hint: "metal precioso"
+    },
+    {
+        word: "Amazon",
+        hint: "sitio de compras en línea"
+    },
+    {
+        word: "php",
+        hint: "Hypertext Preprocessor"
+    },
+    {
+        word: "virus",
+        hint: "programa malicioso"
+    },
+    {
+        word: "twitter",
+        hint: "red social de noticias"
+    },
+    {
+        word: "celular",
+        hint: "dispositivo móvil"
+    },
+    {
+        word: "avatar",
+        hint: "pelicula de ciencia ficción"
+    },
+    {
+        word: "gif",
+        hint: "formato de imagen"
+    },
+    {
+        word: "mp3",
+        hint: "formato de audio"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "tierra",
+        hint: "tercer planeta del sistema solar"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "ajedrez",
+        hint: "juego de mesa popular"
+    },
+    {
+        word: "github",
+        hint: "plataforma para alojar código"
+    },
+    {
+        word: "png",
+        hint: "formato de imagen"
+    },
+    {
+        word: "ram",
+        hint: "memoria de acceso aleatorio"
+    },
+    {
+        word: "java",
+        hint: "lenguaje de programación"
+    },
+    {
+        word: "google",
+        hint: "poderoso motor de búsqueda"
+    },
+    {
+        word: "excel",
+        hint: "hoja de cálculo"
+    },
+    {
+        word: "mysql",
+        hint: "base de datos relacional"
+    },
+    {
+        word: "paint",
+        hint: "editor de imagenes de windows"
+    },
+    {
+        word: "bitcoin",
+        hint: "criptomoneda mas popular"
+    },
+    {
+        word: "toyota",
+        hint: "marca de vehículo"
+    },
+    {
+        word: "marte",
+        hint: "planeta del sistema solar"
+    },
+    {
+        word: "hosting",
+        hint: "servidor web"
+    },
+    {
+        word: "email",
+        hint: "correo electrónico"
+    },
+    {
+        word: "html",
+        hint: "lenguaje de etiquetas de hipertexto"
+    },
+    {
+        word: "gmail",
+        hint: "servicio de correo electrónico"
+    },
+    {
+        word: "zoom",
+        hint: "plataforma de videconferencias"
+    },
+    {
+        word: "apple",
+        hint: "Empresa con logo de manzana"
+    },
+    {
+        word: "jpeg",
+        hint: "un formato de imagen"
+    },
+    {
+        word: "googlear",
+        hint: "acción de buscar en internet"
+    },
+    {
+        word: "instagram",
+        hint: "red social de fotografías"
+    },
+    {
+        word: "Paraguay",
+        hint: "nombre de país con P"
+    },
+    {
+        word: "joker",
+        hint: "enemigo de Batman"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "facebook",
+        hint: "red social más popular"
+    },
+]
